5ee2f736267a53a95384545f3c3a7423f0a7266e
arrararr <asp87@list.ru>
2020-11-08 14:44:52 +0300

    update icon

--- src/components/LoadingIcon/LoadingIcon.tsx
+++ src/components/LoadingIcon/LoadingIcon.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import cn from 'classnames'
 import styles from './LoadingIcon.module.css'
 
-export function LoadingIcon() {
-  return <div className={styles.root}>{new Array(8).fill(<div />)}</div>
+type Props = {
+  className?: string
+}
+
+export const LoadingIcon: React.FC<Props> = ({ className }) => {
+  return (
+    <div className={cn(styles.root, className)}>
+      {new Array(8).fill(<div />)}
+    </div>
+  )
 }
